16824c77378370e638a7b9c6b2a92593f9b26b15
sohaalakhras <sohaalakhras57@gmail.com>
2023-06-24 17:52:46 +0300

    final update

--- main.js
+++ main.js
@@ -1,76 +1,76 @@
 
 const questions = [
-    {
-      text: " Javascript is an _______ language? ",
-      options: [" Object-Oriented", "Object-Based", "Procedural", "None of the above"],
-      answer: 0
-    },
-    {
-      text: "Which of the following keywords is used to define a variable in Javascript?",
-      options: ["var", "let", "Both A and B", "None of the above"],
-      answer: 2
-    },
-    {
-      text: "Which of the following methods is used to access HTML elements using Javascript?",
-      options: ["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the above"],
-      answer: 2
-    }
-    ,
-    {
-      text: "How can a datatype be declared to be a constant type?",
-      options: ["const", "var", "let", "constant"],
-      answer: 0
-    }
-    ,
-    {
-      text: " When an operators value is NULL, the typeof returned by the unary operator is?",
-      options: ["Boolean", "Undefined", "Object", "Integer"],
-      answer: 2
-    }
-    ,
-    {
-      text: "Which of the following are closures in Javascript?",
-      options: ["Variables", "Functions", "Objects", "All of the above"],
-      answer: 3
-    }
-    ,
-    {
-      text: " Javascript is an _______ language? ",
-      options: [" Object-Oriented", "Object-Based", "Procedural", "None of the above"],
-      answer: 0
-    }
-    ,
-    {
-      text: "Which of the following keywords is used to define a variable in Javascript?",
-      options: ["var", "let", "Both A and B", "None of the above"],
-      answer: 2
-    }
-    ,
-    {
-      text: "Which of the following methods is used to access HTML elements using Javascript?",
-      options: ["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the above"],
-      answer: 2
-    }
-    ,
-    {
-      text: "How can a datatype be declared to be a constant type?",
-      options: ["const", "var", "let", "constant"],
-      answer: 0
-    }
-    ,
-    {
-      text: " Javascript is an _______ language? ",
-      options: [" Object-Oriented", "Object-Based", "Procedural", "None of the above"],
-      answer: 0
-    }
-    ,
-    {
-      text: " When an operators value is NULL, the typeof returned by the unary operator is?",
-      options: ["Boolean", "Undefined", "Object", "Integer"],
-      answer: 2
-    }
-    
-  ];
+  {
+    text: " Javascript is an _______ language? ",
+    options: [" Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+    answer: 0
+  },
+  {
+    text: "Which of the following keywords is used to define a variable in Javascript?",
+    options: ["var", "let", "Both A and B", "None of the above"],
+    answer: 2
+  },
+  {
+    text: "Which of the following methods is used to access HTML elements using Javascript?",
+    options: ["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the above"],
+    answer: 2
+  }
+  ,
+  {
+    text: "How can a datatype be declared to be a constant type?",
+    options: ["const", "var", "let", "constant"],
+    answer: 0
+  }
+  ,
+  {
+    text: " When an operators value is NULL, the typeof returned by the unary operator is?",
+    options: ["Boolean", "Undefined", "Object", "Integer"],
+    answer: 2
+  }
+  ,
+  {
+    text: "Which of the following are closures in Javascript?",
+    options: ["Variables", "Functions", "Objects", "All of the above"],
+    answer: 3
+  }
+  ,
+  {
+    text: " Javascript is an _______ language? ",
+    options: [" Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+    answer: 0
+  }
+  ,
+  {
+    text: "Which of the following keywords is used to define a variable in Javascript?",
+    options: ["var", "let", "Both A and B", "None of the above"],
+    answer: 2
+  }
+  ,
+  {
+    text: "Which of the following methods is used to access HTML elements using Javascript?",
+    options: ["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the above"],
+    answer: 2
+  }
+  ,
+  {
+    text: "How can a datatype be declared to be a constant type?",
+    options: ["const", "var", "let", "constant"],
+    answer: 0
+  }
+  ,
+  {
+    text: " Javascript is an _______ language? ",
+    options: [" Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+    answer: 0
+  }
+  ,
+  {
+    text: " When an operators value is NULL, the typeof returned by the unary operator is?",
+    options: ["Boolean", "Undefined", "Object", "Integer"],
+    answer: 2
+  }
+  
+];
 
 
 
@@ -87,6 +87,8 @@ const scoreContainer = document.getElementById('score-container');
 const scoreText = document.getElementById('score');
 const main = document.getElementById('main');
 const username = document.getElementById('fname');
+const divleaderboard = document.getElementById('divleaderboard');
+const btnleaderboard = document.getElementById('leaderboard');
 startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', nextQuestion);
 
@@ -94,83 +96,96 @@ nextButton.addEventListener('click', nextQuestion);
 
 const randomQuestions = []; 
 while (randomQuestions.length < 10) {
-  const randomIndex = questions[Math.floor(Math.random() * questions.length)];
-  
-  if (!randomQuestions.includes(randomIndex)) {
-    randomQuestions.push(randomIndex);
-  }
+const randomIndex = questions[Math.floor(Math.random() * questions.length)];
+
+if (!randomQuestions.includes(randomIndex)) {
+  randomQuestions.push(randomIndex);
+}
 }
 
 
 
 
 function startGame() {
-  startButton.style.display = 'none';
-  main.style.display ='none';
-  questionContainer.style.display = 'block'; 
-  showQuestion();
+startButton.style.display = 'none';
+main.style.display ='none';
+questionContainer.style.display = 'block'; 
+showQuestion();
 }
 
 function showQuestion() {
-  const question = randomQuestions[currentQuestion];
-  questionNumber.textContent = `${currentQuestion + 1}/${randomQuestions.length}`;
-  questionText.textContent = question.text;
-  optionsList.innerHTML = '';
-  fname.style.display = 'none';
-  leaderboard.style.display = 'none'; 
-  for (let i = 0; i < question.options.length; i++) {
-    const option = document.createElement('li');
-    option.innerHTML = "<input type='radio' />" + question.options[i];
-    option.addEventListener('click', () => checkAnswer(i)); 
-    optionsList.appendChild(option);
-  }
+const question = randomQuestions[currentQuestion];
+questionNumber.textContent = `${currentQuestion + 1}/${randomQuestions.length}`;
+questionText.textContent = question.text;
+optionsList.innerHTML = '';
+fname.style.display = 'none';
+leaderboard.style.display = 'none'; 
+for (let i = 0; i < question.options.length; i++) {
+  const option = document.createElement('li');
+  option.innerHTML = "<input type='radio' />" + question.options[i];
+  option.addEventListener('click', () => checkAnswer(i)); 
+  optionsList.appendChild(option);
+}
 }
 
 function checkAnswer(selectedIndex) {
-  const question = randomQuestions[currentQuestion];
+const question = randomQuestions[currentQuestion];
 
-  if (selectedIndex === question.answer) {
-    score++;
-  }
+if (selectedIndex === question.answer) {
+  score++;
+}
 
-  nextButton.style.display = 'block';
+nextButton.style.display = 'block';
 }
 
 function nextQuestion() {
-  currentQuestion++;
+currentQuestion++;
 
-  if (currentQuestion < randomQuestions.length) {
-    showQuestion();
-    nextButton.style.display = 'none';
-  } else {
-    endGame();
-  }
+if (currentQuestion < randomQuestions.length) {
+  showQuestion();
+  nextButton.style.display = 'none';
+} else {
+  endGame();
+}
 }
 
 function endGame() {
-  questionContainer.style.display = 'none';
-  scoreContainer.style.display = 'block';
-  scoreText.textContent = score;
-  nextButton.style.display = 'none'; 
-  leaderboard.style.display = 'none'; 
-  addToLeaderboard(username.value, score);
+questionContainer.style.display = 'none';
+scoreContainer.style.display = 'block';
+scoreText.textContent = score;
+nextButton.style.display = 'none'; 
+leaderboard.style.display = 'none'; 
+addToLeaderboard(username.value, score);
 }
 
 
 
 //Local storage
-
 function addToLeaderboard(username, score) { 
-  localStorage.setItem("questions", JSON.stringify(questions));
-  let leaderboard = JSON.parse(localStorage.getItem('leaderboard'));
+localStorage.setItem("questions", JSON.stringify(questions));
+let leaderboard = JSON.parse(localStorage.getItem('leaderboard'));
 
-  if (!leaderboard) {
-    leaderboard = [];
-  }
-  
-  leaderboard.push({ username, score });
-  localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
-  
+if (!leaderboard) {
+  leaderboard = [];
+}
+
+leaderboard.push({ username, score });
+localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
+
 }
 
 
+btnleaderboard.addEventListener('click',(event)=>{
+event.preventDefault();
+main.style.display ='none';
+divleaderboard.style.display ='block';
+let leaderboard = JSON.parse(localStorage.getItem('leaderboard'));
+if (leaderboard && leaderboard.length > 0) {
+  let leaderboardList = document.getElementById('leaderboard-list');
+  leaderboard.forEach(entry => {
+    let listItem = document.createElement('li');
+    listItem.textContent = `${entry.username}: ${entry.score}`;
+    leaderboardList.appendChild(listItem);
+  });
+}
+})
